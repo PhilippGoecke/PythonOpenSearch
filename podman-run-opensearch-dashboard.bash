c81a5a2f83877241460cae4f3a3634e40d258340
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-19 14:36:10 +0100

    Fix image name for OpenSearch Dashboard

--- podman-run-opensearch-dashboard.bash
+++ podman-run-opensearch-dashboard.bash
@@ -1,4 +1,4 @@
-podman run -d --name opensearch-dashboard --publish 5601:5601 -e OPENSEARCH_HOSTS='["https://host.containers.internal:9200"]' docker.io/opensearchproject/opensearch-dashboard:3.4.0
+podman run -d --name opensearch-dashboard --publish 5601:5601 -e OPENSEARCH_HOSTS='["https://host.containers.internal:9200"]' docker.io/opensearchproject/opensearch-dashboards:3.4.0
 
 echo "Access it at: http://localhost:5601"
 echo "Default credentials: admin / MyStrongPassword123!"
